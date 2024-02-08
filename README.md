2807ab0a5a173f3617f4e88473f9ee6451eff890
Stefany Francine Pasold <58121215+stefanypasold@users.noreply.github.com>
2024-02-08 09:33:36 -0300

    codigo final

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # nlw-expert
+```js
+//
+const mensagem = "Bom te ver por aqui! "
+alert(mensagem + (10 * 100) + " abraços")
+// 
+```
+## quando inserido o name="item" não permitirá selecionar mais de uma opção no radio será identificado o value selecionado
+
+## Array ou Vetores [] agrupamento de valores
+
+## Objetos
+
+## CSS - estilização da página para visualmente ficar mais atrativo e melhorar a exp do usuário
---
+++ index.js
@@ -0,0 +1,134 @@
+//decription list - dl description term - dt
+//variáveis são valores que não mudam
+// [] array ou vetores são conjuntos de dados de qualquer tipo string, number, boolean
+
+const perguntas = [
+    {
+      pergunta: "Qual é a função do método 'charAt()' em JavaScript?",
+      respostas: [
+        "Retorna o primeiro índice de uma substring encontrada em uma string.",
+        "Retorna o caractere na posição especificada de uma string.",
+        "Retorna o número de caracteres em uma string.",
+      ],
+      correta: 1
+    },
+    {
+      pergunta: "Qual declaração é usada para criar uma variável em JavaScript?",
+      respostas: [
+        "var",
+        "int",
+        "let",
+      ],
+      correta: 0
+    },
+    {
+      pergunta: "Qual desses métodos JavaScript é usado para remover o último elemento de um array e retorna o elemento removido?",
+      respostas: [
+        "pop()",
+        "shift()",
+        "splice()",
+      ],
+      correta: 0
+    },
+    {
+      pergunta: "Como você chama uma função chamada 'myFunction'?",
+      respostas: [
+        "chamar myFunction()",
+        "call myFunction()",
+        "myFunction()",
+      ],
+      correta: 2
+    },
+    {
+      pergunta: "O que o método 'toFixed()' faz em JavaScript?",
+      respostas: [
+        "Retorna o número de casas decimais de um número.",
+        "Arredonda um número para o inteiro mais próximo.",
+        "Formata um número usando notação científica.",
+      ],
+      correta: 0
+    },
+    {
+      pergunta: "O que 'NaN' representa em JavaScript?",
+      respostas: [
+        "Not a Name",
+        "Not a Null",
+        "Not a Number",
+      ],
+      correta: 2
+    },
+    {
+      pergunta: "Qual operador é usado para atribuição de valores em JavaScript?",
+      respostas: [
+        "=",
+        "==",
+        "+=",
+      ],
+      correta: 0
+    },
+    {
+      pergunta: "Qual método JavaScript é usado para selecionar elementos HTML por sua classe?",
+      respostas: [
+        "getElementByClass()",
+        "selectElementsByClass()",
+        "getElementsByClassName()",
+      ],
+      correta: 2
+    },
+    {
+      pergunta: "Qual é o resultado de 5 + '3' em JavaScript?",
+      respostas: [
+        "8",
+        "53",
+        "Erro",
+      ],
+      correta: 1
+    },
+    {
+      pergunta: "Qual método JavaScript é usado para arredondar um número para o inteiro mais próximo?",
+      respostas: [
+        "Math.round()",
+        "Math.floor()",
+        "Math.ceil()",
+      ],
+      correta: 0
+    },
+  ];
+  
+  const quiz = document.querySelector('#quiz')
+  const template = document.querySelector('template')
+  
+  const corretas = new Set()
+  const totalDePerguntas = perguntas.length
+  const mostrarTotal = document.querySelector('#acertos span')
+  mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+  
+  // loop ou laço de repetição - vai realizar uma interação para cada pergunta de dentro da variável que está em of variable 
+  for(const item of perguntas) {
+  
+    //clonar do template
+    const quizItem = template.content.cloneNode(true)
+    quizItem.querySelector('h3').textContent = item.pergunta
+  
+    for(const resposta of item.respostas){
+      const dt = quizItem.querySelector('dl dt').cloneNode(true)
+      dt.querySelector('span').textContent = resposta
+      dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf)
+      dt.querySelector('input').value = item.respostas.indexOf(resposta)
+      dt.querySelector('input').onchange = (event) => {
+        const isCorrect = event.target.value ==item.correta
+        if(isCorrect){
+          corretas.add(item)
+        }
+        mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+      }
+  
+      quizItem.querySelector('dl').appendChild(dt)
+    }
+  
+    quizItem.querySelector('dl dt').remove()
+  
+    //coloca a pergunta na tela
+    quiz.appendChild(quizItem)
+  
+  }
